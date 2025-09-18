d5ad1af38e8217805b6cd2c62f72c7c3da38d330
datmai <q.dat2710@gmail.com>
2025-09-19 06:04:54 +0700

    add waiting postgres db

--- src/dotFitness.WorkoutTracker/dotFitness.Aspire.AppHost/AppHost.cs
+++ src/dotFitness.WorkoutTracker/dotFitness.Aspire.AppHost/AppHost.cs
@@ -6,11 +6,13 @@ var postgresDb = postgres.AddDatabase("dotFitnessDb-pg");
 var mongo = builder.AddMongoDB("mongo")
     .WithLifetime(ContainerLifetime.Persistent)
     .WithMongoExpress();
+
 var mongoDb = mongo.AddDatabase("dotFitnessDb-mongo");
 var webApi = builder.AddProject<Projects.dotFitness_Api>("webApi")
     .WithReference(postgresDb)
     .WithReference(mongoDb)
-    .WaitFor(mongoDb);
+    .WaitFor(mongoDb)
+    .WaitFor(postgresDb);
 
 var frontend = builder.AddNpmApp("frontend", "../ClientApp", "dev")
     .WithReference(webApi)
